d7eb9df9a511c83715d48b8a7f18a5a9ad3ce86f
Hannah_Brannan <hannahbrannan@gmail.com>
2016-07-20 15:33:02 -0700

    Still handling syntax errors,

--- server/utils.js
+++ server/utils.js
@@ -25,24 +25,26 @@ module.exports = {
   },
   postPhoto: function (req) {
     console.log('weeeee! in postPhoto', req.user_id);
-    var postedUser = req.body.headers;
     var user_id = postedUser.user_id;
     // var photoPost = schemas.Photo.
   },
   getPhotoCaptions: function  (photo) {
+    console.log('hi');
     // function to get all images/captions for the day
     // includes: caption text, image URL, vote count, user who posted ?
   },
   postCaption: function (caption) {
+        var user_id = postedUser.user_id;
     // function to post a user-created caption
   },
   upVoteCaption: function (caption) {
     // function to upvote a particular caption
+        var user_id = postedUser.user_id;
   }, 
   downVoteCaption: function (caption) {
-
+    var user_id = postedUser.user_id;
   },
   displayVotes: function (caption) {
-
+    var user_id = postedUser.user_id;
   }
 };
