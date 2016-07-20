b3a86ecc1429eb33a62b13e48ec835c8af25879c
Hannah_Brannan <hannahbrannan@gmail.com>
2016-07-20 12:32:06 -0700

    Add an err log

--- server/utils.js
+++ server/utils.js
@@ -17,7 +17,7 @@ module.exports = {
         return results;
       })
       .catch(function (err) {
-        console.log('Suxxx 4 U aint NO results, k?');
+        console.log('Suxxx 4 U aint NO results, k?', err);
       });
   },
   getDailyPhoto: function (user) {
