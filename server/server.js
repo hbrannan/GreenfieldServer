92ba8fe3e6a5fe1bf030a3c9e222dbe7a72f02f4
Hannah_Brannan <hannahbrannan@gmail.com>
2016-07-20 15:16:53 -0700

    Hoping to see a responsive heroku

--- server/server.js
+++ server/server.js
@@ -31,12 +31,12 @@ app.post('/users/create', function (req, res) {
 });
 
 app.get('/photos', function (req, res) {
-	// res.send(utils.getAllPhotos());
+	res.send(utils.getAllPhotos());
 });
 
 app.post('/photos', function (req, res) {
 	utils.postPhoto(req);
-	// res.send('bichez b postin up');
+	res.send('bichez b postin up');
 
 });
 
