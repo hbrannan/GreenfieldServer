94a3fb600a615dc91b6cbb0c38d38446349f4847
Hannah_Brannan <hannahbrannan@gmail.com>
2016-07-20 17:16:01 -0700

    Doi app.use NOT app.user

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.use(express.static(__dirname + '/../dummy.html'));
 
 /////////MIDDLEWARE//////////
 app.use(bodyParser.json());
-app.user(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: true}));
 
 
 ///////////ROUTES///////////
