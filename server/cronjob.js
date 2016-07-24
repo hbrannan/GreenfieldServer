c4f8444b7119797e1b0d8f89011c49c6c463c64a
Hannah_Brannan <hannahbrannan@gmail.com>
2016-07-24 15:11:22 -0700

    start with cron undertake

--- server/cronjob.js
+++ server/cronjob.js
@@ -4,22 +4,30 @@ const utils = require('./server/utils.js');
 const fs = require('fs');
 
 module.exports = new cron('0 0 0 * * *', function () {
-    // var allPhotos = utils.getAllPhotos(); //{{}}
-    // allPhotos = JSON.parse(allPhotos);
-    // console.log(allPhotos);
+    fs.readFile('./usedPhotosOfTheDay.txt', 'utf8', function(err, data){
+        //split by new 
+    })
+    .then(function (lastId) {
+        db.findAll({where: ['id > ?', lastId], order: 'id'})
+    })
+        //query db for all photos w/ id greater than last fsID
+    .then (function (allUnusedPhotos){
+            //if we get some back:   EG .length ? is not falsy?
+        if (allUnusedPhotos) {
+            //loop thru our results obj
+            //map it into an array
+            //sort the array
+            //set the newPhotoOfTheDayId of the first id in the file
+        } else {
+            //if we don't, you ARE at the greatest ID. SO: 
+            //set newPhotoOfTheDayId that is at the firstLine of usedPOD.txt
+              //wipe fsfile clean
+        }
+    })
+    .then(function (){
+        fs.appendFile('./usedPhotosOfTheDay.txt', newPhotoOfTheDayId, function (){
+            ///make sure to write with newLine
+        });
+      })
 
-    // var lowestId = id;
-    //   //loop thru all 
-    // for (var prop in allPhotos){
-    //     //if current id is ONE greater than id && exists
-    //     if (allPhotos[prop][id] > id){
-    //         //RETURN OUT OF THE LOOP
-    //         //set the id to a new id
-    //     } else {
-    //         //reset id to be the lowest id in the group
-    //         if(allPhotos[prop][id] < id) {
-    //             lowestId = allPhotos[prop][id];
-    //         }
-    //     }
-    // }
 });
