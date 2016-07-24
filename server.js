02d8b4e35ae72524582be3c8ee2982f0d7966f6f
Hannah_Brannan <hannahbrannan@gmail.com>
2016-07-24 15:32:07 -0700

    add cron.start()

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(require('express-promise')());
 
-cronJob;
+cronJob.start();
 ///////////ROUTES///////////
 
 ///HOMEPAGE
--- server/cronjob.js
+++ server/cronjob.js
@@ -7,7 +7,7 @@ module.exports = new cron('10 0 0 * * *', function () {
     fs.readFile('./usedPhotosOfTheDay.txt', 'utf8', function(err, data){
         return data.toString().split('\n');
         //this should return an array of lines
-    })
+    })g
     .then(function(linesArray){
         //get last one
         return linesArray[linesArray.length-1];
