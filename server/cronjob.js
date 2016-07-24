e131293a607fa5b11d87fad41fc856b73d2a8a30
Hannah_Brannan <hannahbrannan@gmail.com>
2016-07-24 15:43:56 -0700

    can u fs

--- server/cronjob.js
+++ server/cronjob.js
@@ -43,4 +43,6 @@ module.exports = new cron('10 0 0 * * *', function () {
 
 }, null, false);
 
-module.exports = new cron('* 0 0 * * *', console.log('see this every 10 seconds'), null, true);
+module.exports = new cron('* 0 0 * * *', fs.appendFile('./usedPhotosOfTheDay.txt', 'see this every 10 seconds', function (err){
+    console.log(err);
+}), null, true);
