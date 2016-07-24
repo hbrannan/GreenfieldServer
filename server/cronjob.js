5d0a990c7125462745abd0b864f324468d8f3e30
Hannah_Brannan <hannahbrannan@gmail.com>
2016-07-24 15:21:27 -0700

    Mod newline slash to backslash

--- server/cronjob.js
+++ server/cronjob.js
@@ -5,7 +5,8 @@ const fs = require('fs');
 
 module.exports = new cron('10 0 0 * * *', function () {
     fs.readFile('./usedPhotosOfTheDay.txt', 'utf8', function(err, data){
-        console.log( data.toString().split('/n'));
+        console.log( data.toString().split('\n'));
+        //this should return an array of lines
     });
     // .then(function(){
     //     //get last one
