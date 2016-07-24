6cc3b3fed55a780667fea096bdd20ee404cf0c1f
Hannah_Brannan <hannahbrannan@gmail.com>
2016-07-24 15:48:02 -0700

    Stop the start in server.yas

--- server.js
+++ server.js
@@ -27,7 +27,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(require('express-promise')());
 
-cronJob.start();
+// cronJob.start();
+cronJob;
 ///////////ROUTES///////////
 
 ///HOMEPAGE
