32af27dbded1d38b418df24eb17de5d2ff8a74dd
Hannah_Brannan <hannahbrannan@gmail.com>
2016-07-25 15:25:38 -0700

    Try console log gin

--- server/cronjob.js
+++ server/cronjob.js
@@ -25,6 +25,7 @@ var updateDailyPhoto = function () {
         });
      })
      .then(function(allUnusedPhotos){
+        console.log(allUnusedPhotos);
         var arrayOfPhotos = allUnusedPhotos.dataValues;
         var sortedArr = arrayOfPhotos.sort(function(a,b){
             return a.id - b.id;
