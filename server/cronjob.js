72ad691341b0a6d6c36d967220bf9fac2ae2073c
Hannah_Brannan <hannahbrannan@gmail.com>
2016-07-25 15:05:32 -0700

    try order again

--- server/cronjob.js
+++ server/cronjob.js
@@ -19,7 +19,7 @@ var updateDailyPhoto = function () {
      })
      .then(function(lastId){
         //do a db fetch
-        console.log('dem boyz sux, amiright', db.Photo.findAll({where:{'id > ?', lastId}}));
+        console.log('dem boyz sux, amiright', db.Photo.findAll({where:['id > ?', lastId]}));
      })
      .catch(function(err){
         console.log(err);
